b3647bf4e6e458c5b2138b73ffb759e87101a3c5
Francisco Cobas <fcarocena@gmail.com>
2021-08-25 19:58:44 -0300

    First implementation of SOcialOauth with Supabase 🥳

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { createClient } from '@supabase/supabase-js';
+
+import TwitterIcon from './images/twitter.svg';
+import GithubIcon from './images/github-sign.svg';
+
+type UserMetada = {
+  avatar_url: string;
+  full_name: string;
+  user_name: string;
+};
 
 function App() {
+  const supabaseClient = useRef<any>();
+  const [user, setUser] = useState<UserMetada | undefined>();
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_SUPABASE_URL);
+    if (
+      process.env.REACT_APP_SUPABASE_URL &&
+      process.env.REACT_APP_SUPABASE_ANON_KEY
+    ) {
+      supabaseClient.current = createClient(
+        process.env.REACT_APP_SUPABASE_URL,
+        process.env.REACT_APP_SUPABASE_ANON_KEY
+      );
+      const getUser = async () => {
+        const supabaseUser = supabaseClient.current.auth.user();
+        console.log(supabaseUser);
+        if (supabaseUser) setUser(supabaseUser.user_metadata);
+      };
+      getUser();
+    }
+  }, []);
+
+  const loginWithGithub = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const { user, session, error } = await supabaseClient.current.auth.signIn({
+      provider: 'github',
+    });
+  };
+  const loginWithTwitter = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const { user, session, error } = await supabaseClient.current.auth.signIn({
+      provider: 'twitter',
+    });
+  };
+
+  const logout = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const { error } = await supabaseClient.current.auth.signOut();
+    if (error) {
+      console.log(error);
+      return;
+    }
+    setUser(undefined);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='login-container'>
+      <form>
+        <button onClick={loginWithGithub}>
+          <img src={GithubIcon} alt='Icono de Github' />
+          Login with Github
+        </button>
+        <button onClick={loginWithTwitter}>
+          <img src={TwitterIcon} alt='Icono de Twitter' />
+          Login with Twitter
+        </button>
+        <button onClick={logout}>Logout</button>
+      </form>
+      {user && (
+        <div className='user-information'>
+          <p>
+            <img src={user.avatar_url} alt='User Avatar' />
+          </p>
+          <p>
+            <b>Full Name:</b> {user.full_name}
+          </p>
+          <p>
+            <b>Username:</b> {user.user_name}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
